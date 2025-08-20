a1f37b5ea7d25ac96e269bba3331d00c6cf86143
Виктор Кузинов <96018937+VictorKuzinov@users.noreply.github.com>
2025-08-20 14:15:59 +0500

    Update database.py

--- database.py
+++ database.py
@@ -5,11 +5,12 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite+aiosqlite:///./app.db"
 engine = create_async_engine(
-    DATABASE_URL, 
-    connect_args={"check_same_thread": False}
+    DATABASE_URL, connect_args={"check_same_thread": False}
 )
 # expire_on_commit=False will prevent attributes from being expired
 # after commit.
-async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+async_session = sessionmaker(engine, 
+                             expire_on_commit=False, 
+                             class_=AsyncSession)
 
 Base = declarative_base()
