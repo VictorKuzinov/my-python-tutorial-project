8465d66041d8f2025c5cd670ec22ce45b4acc060
Виктор Кузинов <96018937+VictorKuzinov@users.noreply.github.com>
2025-08-21 11:39:23 +0500

    Update database.py

--- database.py
+++ database.py
@@ -3,9 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 DATABASE_URL = "sqlite+aiosqlite:///./app.db"
-engine = create_async_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_async_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 async_session = sessionmaker(
     engine, expire_on_commit=False, class_=AsyncSession
 )
