7df8ea81b86ec52f0e8c96b7c17d228e78580ec7
Виктор Кузинов <96018937+VictorKuzinov@users.noreply.github.com>
2025-08-21 15:50:23 +0500

    Update database.py
    Signed-off-by: Виктор Кузинов <96018937+VictorKuzinov@users.noreply.github.com>

--- database.py
+++ database.py
@@ -1,12 +1,10 @@
 # database.py
-from typing import cast
-
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.orm import declarative_base
 
 DATABASE_URL = "sqlite+aiosqlite:///./app.db"
 engine = create_async_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
-async_session = cast(sessionmaker, sessionmaker(engine, expire_on_commit=False, class_=AsyncSession))
+async_session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 Base = declarative_base()
