918d9ac6e5bcce5228cfe4548d8ec2697459c14f
Victor K <victor_kuzinov@mail.ru>
2025-08-25 11:42:14 +0500

    Add changes isort from master

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
+from httpx import ASGITransport, AsyncClient
 import pytest_asyncio
-from httpx import AsyncClient, ASGITransport
+
 from my_python_tutorial_project.main import app
+
 
 @pytest_asyncio.fixture
 async def client():
